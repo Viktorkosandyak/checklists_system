55d5a95a871f4d2080e67356697f6e81eac566ab
Viktorkosandyak <kosandyak1996@gmail.com>
2019-06-13 19:26:20 +0300

    fix db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
+User.create(email: 'admin@gmail.com', password: 'Admin1234!', password_confirmation: 'Admin1234!', admin: true)
+
 10.times do
-  Checklist.create!(title: Faker::Name.name, description: Faker::Lorem.paragraph)
+  User.create(email: Faker::Internet.email, password: Faker::Internet.password, admin: false)
+end
+
+
+10.times do
+  Checklist.create!(title: Faker::Name.name, description: Faker::Lorem.paragraph, parent: true, status: 'published')
 end
 
 20.times do
